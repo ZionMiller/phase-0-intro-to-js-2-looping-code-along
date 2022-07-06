e40beab84373445a3df895ef32fc7c98e7571823
Zion Miller <djzionmiller@gmail.com>
2022-07-06 08:34:41 -0700

    commit tree 4fc57fee37d305feba5c5c2d78ed86f297ee2e63

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+// function writeCards(names, event){
+//     for (let i = 0; i < names.length; i++){
+//     names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+//   }
+//     return names;
+//   }
+  
+// function countdown(number) {
+//     let countdown = 0;
+//     while (number >= countdown) {
+//         console.log(number--);
+//     }
+// }
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return messages;
+}
+writeCards(messages);
+
+function countDown(i) {
+    for(let countDown = 0; countDown <= 10; countDown++) {
+        console.log(countDown);
+    }
+}
